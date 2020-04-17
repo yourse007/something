424aca529ea0fac329712dd4f6b9a782d0f9e33d
richie <liu06281996@163.com>
2020-04-17 23:15:25 +0800

    format code

--- src/com/algorithm/leetcode/TwoSum.java
+++ src/com/algorithm/leetcode/TwoSum.java
@@ -6,15 +6,15 @@ import java.util.Map;
 
 /**
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
- *
+ * <p>
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
- *
+ * <p>
  *  
- *
+ * <p>
  * 示例:
- *
+ * <p>
  * 给定 nums = [2, 7, 11, 15], target = 9
- *
+ * <p>
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
  * 所以返回 [0, 1]
  */
@@ -33,13 +33,6 @@ public class TwoSum {
         }
         return null;
     }
-
-
-
-
-
-
-
 
 
     /**
@@ -91,7 +84,7 @@ public class TwoSum {
      * 随机生成指定长度的数组
      *
      * @param arrayLength 数组长度
-     * @param max 随机元素的最大值
+     * @param max         随机元素的最大值
      * @return
      */
     public static int[] generateTestCase(int arrayLength, int max) {
