9293c13489fe8703502aff076c4c1ba2226c671d
richie <liu06281996@163.com>
2020-04-17 23:13:40 +0800

    两数之和的测试用例程序

--- src/com/algorithm/leetcode/TwoSum.java
+++ src/com/algorithm/leetcode/TwoSum.java
@@ -17,7 +17,6 @@ import java.util.Map;
  *
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
  * 所以返回 [0, 1]
- *
  */
 public class TwoSum {
 
@@ -32,13 +31,75 @@ public class TwoSum {
                 map.put(nums[i], i);
             }
         }
-        return new int[]{0, 0};
+        return null;
     }
 
 
+
+
+
+
+
+
+
+    /**
+     * 测试程序
+     * 生成随机长度的数组，为其中每个元素生成随机值
+     */
     public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        System.out.println(Arrays.toString(twoSum(nums, 9)));
+        // 测试用例的个数
+        int testCaseCount = 10000;
+
+        // 每个测试数组的最大长度
+        int arrayMaxLength = 1000;
+
+        // 数组中元素的最大值
+        int elementMaxValue = 1000;
+
+        int arrayLength = 0, nums[] = new int[0], target = 0;
+
+        try {
+            for (int i = 0; i < testCaseCount; i++) {
+                arrayLength = (int) (Math.random() * arrayMaxLength) + 1;
+                nums = generateTestCase(arrayLength, elementMaxValue);
+                target = nums[(int) (Math.random() * arrayLength)];
+
+                int[] result = twoSum(nums, target);
+
+                if (result != null) {
+                    System.out.println(i + "  " + Arrays.toString(result) + " : " + nums[result[0]] + " + " + nums[result[1]] + " == "
+                            + target + " ? " + (nums[result[0]] + nums[result[1]] == target));
+
+                    // 打印验证未通过的测试用例
+                    if (nums[result[0]] + nums[result[1]] != target) {
+                        System.err.println(Arrays.toString(nums));
+                    }
+                } else {
+                    System.out.println(i + "  null");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(arrayLength);
+            System.err.println(Arrays.toString(nums));
+            System.err.println(target);
+        }
+
+    }
+
+    /**
+     * 随机生成指定长度的数组
+     *
+     * @param arrayLength 数组长度
+     * @param max 随机元素的最大值
+     * @return
+     */
+    public static int[] generateTestCase(int arrayLength, int max) {
+        int[] testArray = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            testArray[i] = (int) (Math.random() * max);
+        }
+        return testArray;
     }
 
 }
