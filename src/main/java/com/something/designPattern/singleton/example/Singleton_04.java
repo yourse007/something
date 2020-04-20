8e39fe9f6e3921d80281065737f7115c8c47699f
richie <liu06281996@163.com>
2020-04-20 16:49:28 +0800

    dcl补充

--- src/main/java/com/something/designPattern/singleton/example/Singleton_04.java
+++ src/main/java/com/something/designPattern/singleton/example/Singleton_04.java
@@ -9,6 +9,9 @@ package com.something.designPattern.singleton.example;
  * <p>
  * 这里使用{@code synchronized}保证变量在线程之间的可见性，以及实例化操作的排他性
  * 另外还要依赖于{@code volatile}保证虚拟机不会对指令进行重排序
+ * <p>
+ * DCL单例模式的写法相对来说很麻烦，涉及到的多线程原理比较多，但是这种方式在保证高并发
+ * 下的线程安全之外，也实现了懒加载的效果
  *
  * @see synchronized
  * @see volatile
