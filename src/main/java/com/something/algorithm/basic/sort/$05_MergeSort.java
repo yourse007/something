2a2494ec45ea57c74f5aa6d13f994d895ca04125
richie <liu06281996@163.com>
2020-04-20 19:38:12 +0800

    归并排序

--- src/main/java/com/something/algorithm/basic/sort/$05_MergeSort.java
+++ src/main/java/com/something/algorithm/basic/sort/$05_MergeSort.java
@@ -13,56 +13,90 @@ import java.util.Arrays;
  * <p>
  * 思考：
  * 算法在很多时候都是以空间来换取时间，在归并排序中也不例外。
- * 下面的实现方式，在每次的merge操作中都会新创建一个left+right长度的数组，空间复杂度为O(n2)，如何改进？
- * 是否可以在算法初始时，直接创建和原数组等长的新数组，在merge操作中不断填充这个新数组？这样改进之后空间复杂度可以降低为O(n)
+ * 下面展示的代码实现中，在每次的merge操作中都会新创建一个right - left + 1长度的数组，空间复杂度为O(n2)，如何改进？
  */
 public class $05_MergeSort {
 
-    public static int[] mergeSort(int[] arr) {
-        return mergeSort(arr, 0, arr.length - 1);
+    /**
+     * 归并排序 - 递归方式
+     *
+     * @param arr
+     * @return
+     */
+    public static void mergeSortRecursively(int[] arr) {
+        mergeSort(arr, 0, arr.length - 1);
     }
 
-    private static int[] mergeSort(int[] arr, int left, int right) {
+    private static void mergeSort(int[] arr, int left, int right) {
         if (left < right) {
             // 将数组一分为二
             int middle = left + ((right - left) >> 1);
             // 对左半部分排序
-            int[] leftArr = mergeSort(arr, left, middle);
+            mergeSort(arr, left, middle);
             // 对右半部分排序
-            int[] rightArr = mergeSort(arr, middle + 1, right);
+            mergeSort(arr, middle + 1, right);
             // 合并左右两部分
-            return merge(leftArr, rightArr);
+            merge(arr, left, middle, right);
         }
-
-        System.out.println(left == right);
-        return new int[]{arr[left]};
     }
 
-    private static int[] merge(int[] leftArr, int[] rightArr) {
-        // 申请一个新的数组，长度为左右两数组的长度之和
-        int[] mergeArr = new int[leftArr.length + rightArr.length];
-        int leftIndex = 0, rightIndex = 0, mergeIndex = 0;
-        while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+    private static void merge(int[] arr, int left, int middle, int right) {
+        int[] temp = new int[right - left + 1];
+        int leftIndex = left, rightIndex = middle + 1, mergeIndex = 0;
+        while (leftIndex <= middle && rightIndex <= right) {
             // 依次比较左右数组的元素值，较小的值放入新的大数组中
-            mergeArr[mergeIndex++] = leftArr[leftIndex] > rightArr[rightIndex] ? rightArr[rightIndex++] : leftArr[leftIndex++];
+            temp[mergeIndex++] = arr[leftIndex] > arr[rightIndex] ? arr[rightIndex++] : arr[leftIndex++];
         }
 
         // 若左部分数组有剩余元素，则继续填充到大数组中
-        while (leftIndex < leftArr.length) {
-            mergeArr[mergeIndex++] = leftArr[leftIndex++];
+        while (leftIndex <= middle) {
+            temp[mergeIndex++] = arr[leftIndex++];
         }
         // 若右部分数组有剩余元素，则继续填充到大数组中
-        while (rightIndex < rightArr.length) {
-            mergeArr[mergeIndex++] = rightArr[rightIndex++];
+        while (rightIndex <= right) {
+            temp[mergeIndex++] = arr[rightIndex++];
         }
 
-        return mergeArr;
+        for (int i = 0; i < temp.length; i++) {
+            arr[left + i] = temp[i];
+        }
+    }
+
+    /**
+     * 归并排序 - 非递归方式
+     *
+     * @param arr
+     */
+    public static void mergeSortWithLoop(int[] arr) {
+        int length = arr.length;
+        // 非递归方式从最小单位开始合并
+        int mergeSize = 1;
+        while (mergeSize < length) {
+            int left = 0;
+            while (left < length) {
+                int middle = left + mergeSize - 1;
+                if (middle >= length) {
+                    break;
+                }
+                int right = Math.min(middle + mergeSize, length - 1);
+                merge(arr, left, middle, right);
+                left = right + 1;
+            }
+            if (mergeSize > length >> 1) {
+                break;
+            }
+            mergeSize <<= 1;
+        }
     }
 
 
     public static void main(String[] args) {
         int[] arr = {3, 22, -11, 53, 2521, 234};
-        int[] sorted = mergeSort(arr);
-        System.out.println(Arrays.toString(sorted));
+        mergeSortRecursively(arr);
+        System.out.println(Arrays.toString(arr));
+
+        int[] arr1 = {3, 22, -11, 53, 2521, 234};
+        mergeSortWithLoop(arr1);
+        System.out.println(Arrays.toString(arr1));
     }
 }
