d8bffaa93aae833c28010536a6ce3d33c41a2263
richie <liu06281996@163.com>
2020-04-23 09:59:00 +0800

    修改二分查找

--- src/main/java/com/something/algorithm/basic/search/BinarySearch.java
+++ src/main/java/com/something/algorithm/basic/search/BinarySearch.java
@@ -1,5 +1,10 @@
 package com.something.algorithm.basic.search;
 
+import com.something.algorithm.basic.sort.$06_QuickSort;
+import com.something.algorithm.util.ArrayUtil;
+
+import java.util.Arrays;
+
 /**
  * 二分查找算法
  * <p>
@@ -16,10 +21,10 @@ public class BinarySearch {
     public static int binarySearchWithLoop(int[] arr, int target) {
 
         int left = 0;
-        int right = arr.length;
+        int right = arr.length - 1;
         int middle;
 
-        while (left < right) {
+        while (left <= right) {
             // 这里采用位运算，防止数组过大的情况下，出现：left + right > Integer.MAX_VALUE
             middle = left + ((right - left) >> 1);
             if (arr[middle] == target) {
@@ -39,30 +44,43 @@ public class BinarySearch {
 
     /**
      * 递归方式实现
+     *
      * @param arr
      * @param target
      * @return
      */
     public static int binarySearchRecursively(int[] arr, int target, int left, int right) {
-        if (left >= right) {
+        if (left > right) {
             return -1;
         }
-        int middle = left + ((left + right) >> 1);
+        int middle = left + ((right - left) >> 1);
         if (arr[middle] == target) {
-            return  middle;
+            return middle;
         }
         if (arr[middle] > target) {
             return binarySearchRecursively(arr, target, left, middle - 1);
         } else {
-            return binarySearchRecursively(arr, target, middle + 1, left);
+            return binarySearchRecursively(arr, target, middle + 1, right);
         }
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 5, 5, 5};
-        int target = 5;
-        int resultWithLoop = binarySearchWithLoop(arr, 5);
-        int resultWithRecursive = binarySearchRecursively(arr, 5, 0, arr.length);
-        System.out.println(resultWithLoop + " == " + resultWithRecursive + " ? " + (resultWithLoop == resultWithRecursive));
+        // 生成一个随机数组
+        int length = 20;
+        int[] arr = ArrayUtil.generateRandomArray(length);
+        // 对数组排序
+        $06_QuickSort.quickSort(arr);
+        System.out.println("给定数组 ：" + Arrays.toString(arr));
+
+        // 随机给定一个数组的元素
+        int randomIndex = (int) (Math.random() * (length - 1));
+        int target = arr[randomIndex];
+        System.out.println("查找第 " + (randomIndex + 1) + " 个元素 ：" + target);
+
+        // 分别使用两种实现方式的二分查找
+        int resultWithLoop = binarySearchWithLoop(arr, target);
+        int resultWithRecursive = binarySearchRecursively(arr, target, 0, arr.length - 1);
+        System.out.println("递归方式的二分查找 ：" + resultWithRecursive);
+        System.out.println("循环方式的二分查找 ：" + resultWithLoop);
     }
 }
